45f42041ff0b7233ca0cdb439a52b837e12a4c30
ThanuObulisundar <oobulisund01@student.dasd.org>
2025-09-19 13:11:55 -0400

    Finished lab 4

--- lab/Lab - Control Flow.playground/Pages/3. App Exercise - Fitness Decisions.xcplaygroundpage/Contents.swift
+++ lab/Lab - Control Flow.playground/Pages/3. App Exercise - Fitness Decisions.xcplaygroundpage/Contents.swift
@@ -3,7 +3,16 @@
  
  You want your math tutoring app to determine if a student has passed a quiz or not. Create a variable `questionsCorrect` equal to the number of questions that your user has gotten correct. Create a constant `totalPoints` equal to 20 since there are 20 questions on a quiz. Write an if-else statement that will print "You passed, but you should consider studying a bit more." if the student gets 70% of the questions correct. If the student gets 90% or better, print "Great job! Move on to the next topic." If the student scores less than 70%, print "You still need to study this more." (1 point)
  */
+var questionCorrect = 2
+let totalPoints = 20
 
+if(questionCorrect >= 14 && questionCorrect < 18){
+    print("You passed, but you should consider studying a bit more.")
+}else if(questionCorrect >= 18){
+    print("Great job! Move on to the next topic.")
+}else if(questionCorrect < 14){
+    print("You need to study more.")
+}
 
 
 
--- lab/Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
+++ lab/Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
@@ -7,16 +7,32 @@
  
  > Test your code with all 3 of the currentAverage values below. Make sure only 1 is uncommented at a time!
  */
+
+
+
 let targetLowerBound = 70
 let targetUpperBound = 90
 
-let currentAverage = 67
+//let currentAverage = 67
 //let currentAverage = 85
-//let currentAverage = 95
+let currentAverage = 95
 
 //insert your code here
+let isAverage = currentAverage >= targetLowerBound && currentAverage <= targetUpperBound
+let isBelowAverage = currentAverage < targetLowerBound
+let isAboveAverage = currentAverage > targetUpperBound
 
+if(isAverage){
+    print("You're doing great, but try to push it a bit!")
+}else if(isAboveAverage){
+    print("You're on the right track")
+}else if(isBelowAverage){
+    print("You need to study more often.")
+}
 //print the results from each current average here.
+print("Current Average 1: You need to study more often. ")
+print("Current Average 2: You're doing grade, but try to push it a bit!")
+print("Current Average 3: You're on the right track")
 
 
 //: [Previous](@previous)  |  page 5 of 9  |  [Next](@next)
